dedbbabff6024d3162f27223ce18dd9130160f67
Ryan Leonard <rhinoisnot@gmail.com>
2017-01-17 19:48:16 -0500

    Started code structure.

---
+++ Aragonite.js
@@ -0,0 +1,67 @@
+path = require("path");
+
+/**
+ * An entrypoint for the testing server.
+*/
+class Aragonite {
+
+  /**
+   * Default options to Aragonite.
+  */
+  get defaults() {
+    let opts = {};
+    opts.runners = ["aragonite-vbox"];
+    return opts;
+  }
+
+  /**
+   * Prepare a testing server to be started with {@link Aragonite#start}.
+   * @param {Object} opts options to configure the usage of Aragonite.
+   * @param {string[]} [opts.runners=["aragonite-vbox"]] plugins used to start testing environments
+  */
+  constructor(opts) {
+    this.opts = Object.assign({}, this.defaults, opts);
+  }
+
+  /**
+   * Loads the plugins specified in the configuration, and starts the testing server.
+   * @return {Promise} resolves when the testing server is running.
+  */
+  start() {
+    let tasks = [];
+    this.runners = [];
+    for(const plugin of this.opts.runners) {
+      tasks.push(this
+        .loadPlugin(plugin)
+        .then((runner) => {
+          this.runners.push(runner);
+        })
+      );
+    }
+    Promise.all(tasks);
+  }
+
+  /**
+   * Loads a plugin by name.
+   * Searches for plugins bundled with Aragonite, as well as `require`-able plugins
+  */
+  loadPlugin(name) {
+    Promise
+      .resolve()
+      .then(function() {
+        require(path.join(__dirname, name, name));
+      })
+      .catch(function() {
+        require(path.join(__dirname, name));
+      })
+      .catch(function() {
+        require(name)
+      })
+      .then((Plugin) => {
+        new Plugin(this.opts);
+      });
+  }
+
+}
+
+module.exports = Aragonite;
